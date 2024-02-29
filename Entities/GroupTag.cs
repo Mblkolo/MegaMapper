aab07bb50f84606b14e17d2b81d53d408c543e97
Roman Rusakov <rusakov@byndyusoft.com>
2024-02-29 19:32:44 +0500

    Начальная структура

--- Entities/GroupTag.cs
+++ Entities/GroupTag.cs
@@ -1,8 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-namespace MegaMapper.Entities
+﻿namespace MegaMapper.Entities
 {
     public class GroupTag
     {
